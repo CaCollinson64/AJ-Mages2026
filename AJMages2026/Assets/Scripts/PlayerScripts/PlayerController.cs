e23143db682d3f821229efe8050267454e6e3aa4
Lucas Ladan <l.ladan419@mybvc.ca>
2026-05-14 17:59:50 -0600

    Updated to be used for 2 players

--- AJMages2026/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ AJMages2026/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,14 +1,22 @@
 using NUnit.Framework.Constraints;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Mathematics;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerController : MonoBehaviour
 {
-    public UnityEvent<majorInput, minorInput> sendInput;
-    private majorInput leftStickInput = majorInput.none;
-    private minorInput rightStickInput = minorInput.up;
+    public UnityEvent<majorInput, minorInput> sendP1Input;
+    private majorInput p1MajorInput = majorInput.none;
+    private minorInput p1MinorInput = minorInput.up;
+    private bool didMajorP1 = false;
+
+    private majorInput p2MajorInput = majorInput.none;
+    private minorInput p2MinorInput = minorInput.up;
+    private bool didMajorP2 = false;
     public enum majorInput
     {
         move,
@@ -23,11 +31,8 @@ public class PlayerController : MonoBehaviour
         up, down, left, right
     }
 
-    public void OnLeftStick(InputValue input)
+    private majorInput GetMajorInput(Vector2 vectorInput)
     {
-        Debug.Log("K");
-        Vector2 vectorInput = input.Get<Vector2>();
-        vectorInput = math.normalize(vectorInput);
         float xValue = vectorInput.x;
         float yValue = vectorInput.y;
         float combined = Mathf.Abs(yValue) - Mathf.Abs(xValue);
@@ -35,63 +40,100 @@ public class PlayerController : MonoBehaviour
         {
             if (yValue > 0)
             {
-                leftStickInput = majorInput.move;
-            }
-            else if (yValue < 0)
-            {
-                leftStickInput = majorInput.shield;
+                return majorInput.move;
             }
             else
             {
-                leftStickInput = majorInput.none;
+                return majorInput.shield;
             }
         }
         else
         {
             if (xValue > 0)
             {
-                leftStickInput = majorInput.spell;
+                return majorInput.spell;
             }
             else
             {
-                leftStickInput = majorInput.item;
+                return majorInput.item;
             }
+        }
+    }
+
+    private minorInput GetMinorInput(Vector2 vectorInput)
+    {
+        float xValue = vectorInput.x;
+        float yValue = vectorInput.y;
+        float combined = Mathf.Abs(yValue) - Mathf.Abs(xValue);
+        if (combined >= 0)
+        {
+            if (yValue >= 0)
+            {
+                return minorInput.up;
+            }
+            else
+            {
+                return minorInput.down;
+            }
+        }
+        else
+        {
+            if (xValue > 0)
+            {
+                return minorInput.right;
+            }
+            else
+            {
+                return minorInput.left;
+            }
+        }
+    }
+
+    public void On1ButtonLeft(InputValue input)
+    {
+        if (!input.isPressed)
+        { return; }
+
+        if (didMajorP1)
+        {
+            sendP1Input.Invoke(p1MajorInput, p1MinorInput);
+        }
+        else
+        {
+            didMajorP1 = true;
+        }
+    }
+
+    public void OnLeftStick(InputValue input)
+    {
+        Vector2 vectorInput = input.Get<Vector2>();
+        vectorInput = math.normalize(vectorInput);
+        
+        if (!didMajorP1)
+        {
+            p1MajorInput = GetMajorInput(vectorInput);
+        }
+        else
+        {
+            p1MinorInput = GetMinorInput(vectorInput);
+            didMinorP1 = true;
         }
     }
 
     public void OnRightStick(InputValue input)
     {
-        Debug.Log("Ok");
         Vector2 vectorInput = input.Get<Vector2>();
         vectorInput = math.normalize(vectorInput);
-        float xValue = vectorInput.x;
-        float yValue = vectorInput.y;
-        float combined = Mathf.Abs(yValue) - Mathf.Abs(xValue);
-        if (combined >= 0.1)
+
+        if (!didMajorP2)//Replace with didMajorP2
         {
-            if (yValue > 0)
-            {
-                rightStickInput = minorInput.up;
-            }
-            else if (yValue < 0)
-            {
-                rightStickInput = minorInput.down;
-            }
-        }
-        else if (combined < 0.1)
-        {
-            if (xValue > 0)
-            {
-                rightStickInput = minorInput.right;
-            }
-            else
-            {
-                rightStickInput = minorInput.left;
-            }
+            p2MajorInput = GetMajorInput(vectorInput);
+            didMajorP2 = true;
         }
         else
         {
-            sendInput.Invoke(leftStickInput, rightStickInput);
+            p2MinorInput = GetMinorInput(vectorInput);
+            didMinorP2 = true;
         }
     }
 }
